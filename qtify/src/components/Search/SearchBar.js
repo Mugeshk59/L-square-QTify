4fbc6daa2e2ffb1c56c33e99ca7dccdc7735ce5a
Mugeshk59 <mugeshk59@gmail.com>
2024-02-18 19:09:47 +0530

    changes popping spelling p to P and changes placeholder sentence

--- qtify/src/components/Search/SearchBar.js
+++ qtify/src/components/Search/SearchBar.js
@@ -7,7 +7,7 @@ import { ReactComponent as SearchIcon } from "../../assets/search-icon.svg";
 export default function SearchBar(){
         return (
                 <div className={styles.container}>
-                        <input name = "search" className={styles.search} placeholder="search" />
+                        <input name = "search" className={styles.search} placeholder="search an album" />
                         <button className={styles.searchButton}>
                         <SearchIcon/>
                         </button>
